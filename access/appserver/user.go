f2f709d15329933bdecf5d9c5eb0b909fbbf7eb3
alanrong <alanrong@gmail.com>
2017-12-18 16:59:49 +0800

    use role instead of roleconf

--- access/appserver/user.go
+++ access/appserver/user.go
@@ -2,33 +2,14 @@ package main
 
 import (
 	"context"
-	"fmt"
 	"log"
 	"net/http"
-	"os"
 
-	simplejson "github.com/bitly/go-simplejson"
 	"github.com/gin-gonic/gin"
 	"github.com/micro/go-micro/client"
 	"github.com/yuntifree/live-server/accounts"
 	"github.com/yuntifree/live-server/proto/user"
 )
-
-const (
-	confPath = "/data/live/role.json"
-)
-
-func loadRoleConf(path string) *simplejson.Json {
-	file, err := os.Open(path)
-	if err != nil {
-		log.Fatal("open %s failed:%v", path, err)
-	}
-	roleConf, err := simplejson.NewFromReader(file)
-	if err != nil {
-		log.Fatal("parse json failed:%v", err)
-	}
-	return roleConf
-}
 
 func userHandler(c *gin.Context) {
 	action := c.Param("action")
@@ -55,8 +36,6 @@ func userLogin(c *gin.Context) {
 		return
 	}
 	log.Printf("rsp:%+v", rsp)
-	conf := loadRoleConf(confPath)
-	r := fmt.Sprintf("%d", rsp.Role)
 	c.JSON(http.StatusOK, gin.H{"errno": 0, "uid": rsp.Uid,
-		"token": rsp.Token, "roleconf": conf.Get(r)})
+		"token": rsp.Token, "role": rsp.Role})
 }
