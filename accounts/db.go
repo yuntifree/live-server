aeb7be09383ece63b27dc9c885f0716076f38c2b
alanrong <alanrong@gmail.com>
2017-12-25 16:00:08 +0800

    update rds

--- accounts/db.go
+++ accounts/db.go
@@ -2,5 +2,5 @@ package accounts
 
 const (
 	//DbDsn dsn for mysql
-	DbDsn = "access:^yunti9df3b01c$@tcp(rm-wz9sb2613092ki9xn.mysql.rds.aliyuncs.com:3306)/live?charset=utf8"
+	DbDsn = "access:^yunti9df3b01c$@tcp(rm-wz908040rccwbfq3n.mysql.rds.aliyuncs.com:3306)/live?charset=utf8"
 )
